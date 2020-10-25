9e051f0de22e442e2541a69ffb76fd181fe79a6c
Umut Alptekin <amacalptekin@gmail.com>
2020-10-25 15:32:57 +0300

    created the sql file to create the three tables, created server.js file, installed npm mysql and inquirer

---
+++ README.md
@@ -0,0 +1,7 @@
+# Employee Tracker
+
+This project is a system that uses a database to keep track of employees, their roles, departments, salaries and their direct managers.
+
+Through CLI, users can add and view people, departments and roles, as well as update the epmolyee roles. 
+
+This application will use MySQL to hold the data, and Iquirerjs for users to interact with the CLI. 
---
+++ employeeTracker-schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS departmentsDB;
+CREATE database departmentsDB;
+
+USE departmentsDB;
+
+CREATE TABLE departmentsTable (
+  id INT NOT NULL,
+  dept_name VARCHAR(30) NULL,
+  PRIMARY KEY (position)
+);
+
+CREATE TABLE rolesTable (
+    id INT PRIMARY KEY,
+    role_title VARCHAR(30), 
+    salary DECIMAL,
+    department_id INT -- to hold reference to department role belongs to
+);
+
+CREATE TABLE employeeTable (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30) to hold employee first name,
+  last_name VARCHAR(30) to hold employee last name,
+  role_id INT, -- to hold reference to role employee has
+  manager_id INT 
+    /*to hold reference to another employee that manager of the current employee. 
+    This field may be null if the employee has no manager*/
+);
+
+
